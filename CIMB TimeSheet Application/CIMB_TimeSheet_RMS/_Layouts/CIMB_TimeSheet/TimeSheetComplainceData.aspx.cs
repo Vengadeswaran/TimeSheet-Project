b79c3ceddebb359b0f7e958af0ac7011affde3e9
Vengadeswaran <venky@itxsolutionsindia.com>
2011-04-18 10:14:24 +0530

    Added TimeSheet Manager and Grouped By TimeSheet Manager in the TimeSheet Compliance Report

--- CIMB TimeSheet Application/CIMB_TimeSheet_RMS/_Layouts/CIMB_TimeSheet/TimeSheetComplainceData.aspx.cs
+++ CIMB TimeSheet Application/CIMB_TimeSheet_RMS/_Layouts/CIMB_TimeSheet/TimeSheetComplainceData.aspx.cs
@@ -47,8 +47,8 @@ namespace CIMB_TimeSheet_RMS._Layouts.CIMB_TimeSheet
                 int endindex = page;
                 Guid adminguid;
                 adminguid = new Guid("6FF0A657-63BC-4390-8AAF-7EE5CE033088");
-                var rbs = new ReadRBSValues(System.Net.CredentialCache.DefaultNetworkCredentials, rbsurl);
-                //var rbs = new ReadRBSValues(System.Net.CredentialCache.DefaultNetworkCredentials, "http://jump/cimb");
+                //var rbs = new ReadRBSValues(System.Net.CredentialCache.DefaultNetworkCredentials, rbsurl);
+                var rbs = new ReadRBSValues(System.Net.CredentialCache.DefaultNetworkCredentials, "http://jump/cimb");
                 var resuids = rbs.GetBottomLevelResouceUIDs();
                 string filterresource = "(";
                 foreach (var resuid in resuids)
@@ -58,12 +58,13 @@ namespace CIMB_TimeSheet_RMS._Layouts.CIMB_TimeSheet
                 filterresource = filterresource.Substring(0, filterresource.Length - 1) + ")";
                 string gridqry = @"
 SELECT     res.ResourceUID, res.ResourceName, res.RBS, tperiod.PeriodUID, tperiod.PeriodStatusID, tperiod.StartDate, tperiod.EndDate, tperiod.PeriodName,
-                      tperiod.LCID
+                      tperiod.LCID, ISNULL(TM_Name.ResourceName, 'Not Assigned') AS TM_Name
 INTO            [#t1]
-FROM         MSP_TimesheetPeriod AS tperiod CROSS JOIN
-                      MSP_EpmResource_UserView AS res
+FROM         MSP_EpmResource_UserView AS TM_Name RIGHT OUTER JOIN
+                      MSP_EpmResource_UserView AS res ON TM_Name.ResourceUID = res.ResourceTimesheetManagerUID CROSS JOIN
+                      MSP_TimesheetPeriod AS tperiod
 WHERE     (tperiod.StartDate BETWEEN DATEADD(d, - 7,'" + _stdate + @"') AND '" + _enddate + @"') AND (res.ResourceUID IN " + filterresource + @")
-SELECT      [#t1].PeriodUID, [#t1].ResourceUID, [#t1].RBS, [#t1].ResourceName, [#t1].PeriodName,
+SELECT      [#t1].PeriodUID, [#t1].ResourceUID,[#t1].TM_Name, [#t1].RBS, [#t1].ResourceName, [#t1].PeriodName,
 			ISNULl(tstatus.Description,'Not Created') AS [TimeSheet Status], [#t1].StartDate, [#t1].EndDate
 INTO #t2
 FROM        MSP_TimesheetStatus AS tstatus INNER JOIN
@@ -71,7 +72,7 @@ FROM        MSP_TimesheetStatus AS tstatus INNER JOIN
             MSP_TimesheetResource AS tres ON tsheet.OwnerResourceNameUID = tres.ResourceNameUID RIGHT OUTER JOIN
             [#t1] ON [#t1].ResourceUID = tres.ResourceUID AND [#t1].PeriodUID = tsheet.PeriodUID
 drop table #t1
-SELECT ResourceName, PeriodName, [TimeSheet Status] FROM #t2 WHERE [TimeSheet Status] <> 'Approved'
+SELECT TM_Name, ResourceName,PeriodName, [TimeSheet Status] FROM #t2 WHERE [TimeSheet Status] <> 'Approved' ORDER BY TM_Name, ResourceName, PeriodName
 drop table #t2
 ";
                 WindowsImpersonationContext wik = null;
@@ -96,10 +97,11 @@ drop table #t2
                                 s_RowData newrow = new s_RowData();
                                 newrow.id = idx++;
                                 //Tabel Column List -- ResourceName - 0,TimeSheet Period - 1,TimeSheet Period Status - 2
-                                newrow.cell = new string[3];  //total number of columns
-                                newrow.cell[0] = row[0].ToString(); //resource name
-                                newrow.cell[1] = row[1].ToString(); //TimeSheet Period Name
-                                newrow.cell[2] = row[2].ToString(); //TimeSheet Period Status
+                                newrow.cell = new string[4];  //total number of columns
+                                newrow.cell[0] = row[0].ToString(); //TimeSheet Manager name
+                                newrow.cell[1] = row[1].ToString(); //resource name
+                                newrow.cell[2] = row[2].ToString(); //TimeSheet Period Name
+                                newrow.cell[3] = row[3].ToString(); //TimeSheet Period Status
                                 rowsadded.Add(newrow);
                             }
                         }
